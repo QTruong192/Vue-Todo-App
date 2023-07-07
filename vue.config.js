9d50a2ed87e90217aa7a0dc3d6257cd31c43d87c
Gnourt <119716367+Gnourt192@users.noreply.github.com>
2023-07-08 02:08:50 +0700

    push something :<

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath: '/todo/',
   transpileDependencies: true,
 });
+module.exports ={
+  publicPath: '/Vue-Todo-App/',
+}
