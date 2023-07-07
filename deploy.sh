3a0f0e84ce72af42c8f67b59f6470222ea2dcc47
Gnourt <119716367+Gnourt192@users.noreply.github.com>
2023-07-08 02:46:12 +0700

    fix code

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:QTruong192/Vue-Todo-App.git main:gh-pages
+git push -f git@github.com:QTruong192/Vue-Todo-App.git master:gh-pages
 
 cd -
