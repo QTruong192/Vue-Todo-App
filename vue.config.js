791b65060a62ea93014a6b8e117c229e7e7235cd
Gnourt <119716367+Gnourt192@users.noreply.github.com>
2023-07-08 02:21:52 +0700

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
-module.exports ={
-  publicPath: '/Vue-Todo-App/',
-}
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/Vue-Todo-App/" : "/",
+};
